906fd086e2302859a6396f40d52ced5b2f72b11b
Kris OMealy <kris.omealy@tealium.com>
2020-10-14 12:42:25 -0700

    do not do the virtualenv stuff that no longer works

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs Python, pip and virtualenv. Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments."
-version           "1.4.7"
+version           "1.4.8"
 
 depends           "build-essential"
 depends           "yum-epel"
@@ -12,7 +12,7 @@ recipe "python", "Installs python, pip, and virtualenv"
 recipe "python::package", "Installs python using packages."
 recipe "python::source", "Installs python from source."
 recipe "python::pip", "Installs pip from source."
-recipe "python::virtualenv", "Installs virtualenv using the python_pip resource."
+# recipe "python::virtualenv", "Installs virtualenv using the python_pip resource."
 
 %w{ debian ubuntu centos redhat fedora freebsd smartos }.each do |os|
   supports os
